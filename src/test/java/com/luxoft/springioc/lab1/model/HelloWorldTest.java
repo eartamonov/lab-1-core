84f4c05d8b50b3b21afce9eb74d097f7079eaf05
Artamonov <artamonov-e@haulmont.com>
2019-10-16 13:45:36 +0400

    15. Change the application-context.xml in order the person bean to use autowiring by name

--- src/test/java/com/luxoft/springioc/lab1/model/HelloWorldTest.java
+++ src/test/java/com/luxoft/springioc/lab1/model/HelloWorldTest.java
@@ -26,7 +26,7 @@ public class HelloWorldTest {
 
 	@Test
 	public void testInitPerson() {
-		UsualPerson person = (UsualPerson) context.getBean("person", Person.class);
+		UsualPerson person = (UsualPerson) context.getBean("person"); //byName
 
 		System.out.println("->" + person.getCountry().getA());
 //		assertEquals(expectedPerson, person);
